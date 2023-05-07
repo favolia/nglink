19eb2ed137b699de5da88bb035b201acae2c8149
favolia <98775120+favolia@users.noreply.github.com>
2023-05-07 16:22:14 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+const port = 3000;
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+// Menambah data baru
+server.post('/comments', (req, res, next) => {
+  req.body.id = Date.now(); // Tambahkan ID unik
+  next();
+});
+
+// Menggunakan router dari JSON Server
+server.use(router);
+
+// Menjalankan server
+server.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
+
+
